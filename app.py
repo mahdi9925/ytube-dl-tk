6d8d2bbf56e84dcd6f74ab7edfe9833f6ee66b0b
Mahdi Momeni <mahdimomeni178@gmail.com>
2024-03-30 23:29:23 +0330

    better comment

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ import main
 from multiprocessing import Queue
 from multiprocessing import Process
 
-
+# * Main window and its settings
 mw = Tk()  # noqa: F405
 mw.title("YouTube downloader")
 mw.geometry("500x500")
 
-str_variable = StringVar()  # noqa: F405
+url_variable = StringVar()  # noqa: F405
 
 
 def Check_internet_connection_process():
@@ -19,25 +19,28 @@ def Check_internet_connection_process():
 
 
 def downloader(url):
-    print(str_variable.get())
+    print(url_variable.get())
 
 
+# * Create a frame for hold content
 content = ttk.Frame(mw).grid(row=0, column=0)
 
+# * Create a Entry object for get url and its Label
 url_label = ttk.Label(content, text="URL: ", background="#000fff000").grid(
     row=0, column=0
 )
+url = ttk.Entry(content, textvariable=url_variable, width=50).grid(row=0, column=1)
 
-url = ttk.Entry(content, textvariable=str_variable, width=50).grid(row=0, column=1)
-
+# * Create a download button and its style
 btn_download = ttk.Button(
-    content, text="Download", command=lambda: downloader(str_variable.get())
+    content, text="Download", command=lambda: downloader(url_variable.get())
 ).grid(row=1, column=3)
 
 
 if __name__ == "__main__":
-    # create a child process process
+    # * create a child process process for checking internet connectivity
     process = Process(target=Check_internet_connection_process)
     process.start()
 
+    # * Execute the Main Window
     mw.mainloop()
