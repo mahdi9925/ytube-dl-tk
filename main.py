f9ff85aafc537d69e302ce3f9701871ca978d5dc
Mahdi Momeni <mahdimomeni178@gmail.com>
2024-07-19 11:40:50 +0330

    update `main.py`

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ def downloader(url):
 
 
 def check_internet_connection(queue):
-    url = "https://www.wikipedia.org/"
+    url = "https://www.google.com/"
     try:
         response = requests.get(url)
         result = f"HTTP STATUS : {response.status_code} | Connection established"
+        # print(result)
         queue.put(result)
     except requests.exceptions.ConnectionError:
         queue.put("No internet connection")
